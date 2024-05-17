630a019a8637eb47191aa9ba3669f630071c2af9
jrest01 <julian11072000@hotmail.com>
2024-05-17 14:26:48 -0500

    Adding Data Annotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EntityFramework.models;
 
 public class Category
 {
+    [Key]
     public Guid CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(100)]
     public string? Name { get; set; }
     public string? Description { get; set; }
 
--- Models/Task.cs
+++ Models/Task.cs
@@ -1,17 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EntityFramework.models;
 
 public class Task
 {
-
-
+    [Key]
     public Guid TaskId { get; set; }
+    [ForeignKey("CategoryId")]
     public Guid CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(150)]
     public string Title { get; set; }
     public string Description { get; set; }
     public Priority TaskPriority { get; set; }
     public DateTime Date { get; set; }
 
     public virtual Category Category { get; set; }
+
+    [NotMapped]
+    public string Resume { get; set; }
 }
 
 public enum Priority
