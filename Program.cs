77e16b0da792a707504d0fee96fb1365d7ff135d
jrest01 <julian11072000@hotmail.com>
2024-05-20 13:03:57 -0500

    Creating database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Creates a DB InMemory to validate the operation
+//builder.Services.AddDbContext<ProjectContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
+//Connecting with PostgreSQL
+var Configuration = builder.Configuration;
+builder.Services.AddDbContext<ProjectContext>(options => options.UseNpgsql(
+    Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconnection", async ([FromServices] ProjectContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Database on Memory: " + dbContext.Database.IsInMemory());
+});
+
 
 app.Run();
